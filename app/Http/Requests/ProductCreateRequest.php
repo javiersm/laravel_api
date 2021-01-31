63ebb065d4a6f55c3eff304ef1c08f1fa1a89262
javier.santana.mejias@gmail.com <$Tengosuerte1>
2021-01-31 10:51:27 +0100

    Product create

---
+++ app/Http/Requests/ProductCreateRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|unique:products|max:255',
+
+            'detail' => 'required',
+
+            'price' => 'required',
+
+            'stock' => 'required|max:6',
+
+            'discount' => 'required|max:2',
+        ];
+    }
+}
